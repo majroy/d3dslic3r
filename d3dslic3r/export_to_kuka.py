36cda6b1f0ed2d095ca7cf25882bb41c72f83ac3
renankin <renankin@outlook.com>
2024-08-07 13:19:06 +0100

    Iterate over multiple outlines

--- d3dslic3r/export_to_kuka.py
+++ d3dslic3r/export_to_kuka.py
@@ -1,19 +1,8 @@
 import numpy as np
 from d3dslic3r_common import respace_equally
 import matplotlib.pyplot as plt
-
-# import outline already ordered
-A = np.genfromtxt('slice_export/Slice_0_0.txt')
-
-# obtain only x and y coords
-xycoords = np.delete(A, 2, axis=1)
-
-# respace the points and include z vals (fixed)
-interpoints, perimeter, nPts = respace_equally(xycoords, 5.00)
-interpoints = np.insert(interpoints, 2, A[0,2], axis=1)
-
-plt.plot(interpoints[:,0], interpoints[:,1], '.')
-#plt.show()
+from natsort import natsorted
+import os
 
 # write a kuka .SRC and .DAT file
 file_name = 'BunnyProfile'
@@ -60,62 +49,79 @@ fs.write(r';ENDFOLD' '\n')
 fs.write(r'SPTP XHOME WITH $VEL_AXIS[1] = SVEL_JOINT(100.0), $TOOL = STOOL2(FHOME), $BASE = SBASE(FHOME.BASE_NO), $IPO_MODE = SIPO_MODE(FHOME.IPO_FRAME), $LOAD = SLOAD(FHOME.TOOL_NO), $ACC_AXIS[1] = SACC_JOINT(PDEFAULT), $APO = SAPO_PTP(PDEFAULT), $GEAR_JERK[1] = SGEAR_JERK(PDEFAULT), $COLLMON_TOL_PRO[1] = USE_CM_PRO_VALUES(0)' '\n')
 fs.write(r';ENDFOLD' '\n\n')
 
-for i, points in enumerate(interpoints):
-    
-    if i == 0: # write arc on
-        fs.write(fr';FOLD ARCON WDAT{i+1} SPTP P{i+1} Vel=100 % PDAT{i+1} Tool[1]:Welder Base[1] ;%{{PE}}' '\n')
-        fs.write(r';FOLD Parameters ;%{h}' '\n')
-        fs.write(fr';Params IlfProvider=kukaroboter.arctech.arconstandardsptp; Kuka.IsGlobalPoint=False; Kuka.PointName=P{i+1}; Kuka.BlendingEnabled=False; Kuka.MoveDataPtpName=PDAT{i+1}; Kuka.VelocityPtp=100; Kuka.VelocityFieldEnabled=True; Kuka.ColDetectFieldEnabled=True; Kuka.CurrentCDSetIndex=0; Kuka.MovementParameterFieldEnabled=True; IlfCommand=; ArcTech.WdatVarName=WDAT1; ArcTech.Basic=3.3.3.366; ArcTech.Advanced=3.3.1.22' '\n')
-        fs.write(r';ENDFOLD' '\n')
-        fs.write(fr';TRIGGER WHEN DISTANCE = 1 DELAY = ArcGetDelay(#PreDefinition, WDAT{i+1}) DO ArcMainNG(#PreDefinition, WDAT{i+1}, WP{i+1}) PRIO = -1' '\n')
-        fs.write(fr';TRIGGER WHEN DISTANCE = 1 DELAY = ArcGetDelay(#GasPreflow, WDAT{i+1}) DO ArcMainNG(#GasPreflow, WDAT{i+1}, WP{i+1}) PRIO = -1' '\n')
-        fs.write(fr';ArcMainNG(#ArcOnBeforeSplSingle, WDAT{i+1}, WP{i+1})' '\n')
-        fs.write(fr'SPTP XP{i+1} WITH $VEL_AXIS[1] = SVEL_JOINT(100.0), $TOOL = STOOL2(FP{i+1}), $BASE = SBASE(FP{i+1}.BASE_NO), $IPO_MODE = SIPO_MODE(FP{i+1}.IPO_FRAME), $LOAD = SLOAD(FP{i+1}.TOOL_NO), $ACC_AXIS[1] = SACC_JOINT(PPDAT{i+1}), $APO = SAPO_PTP(PPDAT{i+1}), $GEAR_JERK[1] = SGEAR_JERK(PPDAT{i+1}), $COLLMON_TOL_PRO[1] = USE_CM_PRO_VALUES(0)' '\n')
-        fs.write(fr';ArcMainNG(#ArcOnAfterSplSingle, WDAT{i+1}, WP{i+1})' '\n')
-        fs.write(r';ENDFOLD' '\n\n')
+# get exported files from d3dslic3r
+path = 'slice_export'
+dir_list = os.listdir(path)
 
-        fd.write(fr'DECL stArcDat_T WDAT{i+1}={{WdatId[] "WDAT1",Strike {{JobModeId[] "Job mode",ParamSetId[] "Set1",StartTime 0.0,PreFlowTime 0.0,Channel1 1.00000,Channel2 0.0,Channel3 0.0,Channel4 0.0,Channel5 0.0,Channel6 0.0,Channel7 0.0,Channel8 0.0,PurgeTime 0.0}},Weld {{JobModeId[] "Job mode",ParamSetId[] "Set2",Velocity {(tool_speed/60):.6f},Channel1 1.00000,Channel2 0.0,Channel3 0.0,Channel4 0.0,Channel5 0.0,Channel6 0.0,Channel7 0.0,Channel8 0.0}},Weave {{Pattern #None,Length 4.00000,Amplitude 2.00000,Angle 0.0,LeftSideDelay 0.0,RightSideDelay 0.0}},Advanced {{IgnitionErrorStrategy 1,WeldErrorStrategy 1,SlopeOption #None,SlopeTime 0.0,SlopeDistance 0.0,OnTheFlyActiveOn FALSE,OnTheFlyActiveOff FALSE,OnTheFlyDistanceOn 0.0,OnTheFlyDistanceOff 0.0}}}}' '\n')
-        fd.write(fr'DECL stArcDat_T WP{i+1}={{WdatId[] "WP{i+1}",Info {{Version 303030366}},Strike {{SeamName[] " ",PartName[] " ",SeamNumber 0,PartNumber 0,DesiredLength 0.0,LengthTolNeg 0.0,LengthTolPos 0.0,LengthCtrlActive FALSE}},Advanced {{BitCodedRobotMark 0}}}}' '\n')
-        fd.write(fr'DECL FRAME XP{i+1}={{X {points[0]:.6f},Y {points[1]:.6f},Z {points[2]:.6f},A {tool_angle[0]:.6f},B {tool_angle[1]:.6f},C {tool_angle[2]:.6f}}}' '\n')
-        fd.write(fr'DECL FDAT FP{i+1}={{TOOL_NO 1,BASE_NO 1,IPO_FRAME #BASE,POINT2[] " "}}' '\n')
-        fd.write(fr'DECL PDAT PPDAT{i+1}={{VEL 100.000,ACC 100.000,APO_DIST 500.000,APO_MODE #CDIS,GEAR_JERK 100.000,EXAX_IGN 0}}' '\n\n')
+j = 1
+for file_name in natsorted(dir_list):
+    if file_name[-6:] == '_0.txt': # only outlines
+        A = np.genfromtxt(path + '/' + file_name)
+        xycoords = np.delete(A, 2, axis=1)
 
-    elif i == len(A)-1:
-        # write arc off
-        fs.write(fr';FOLD ARCOFF WDAT{i+1} SLIN P{i+1} CPDAT{i+1} Tool[1]:Welder Base[1] ;%{{PE}}' '\n')
-        fs.write(r';FOLD Parameters ;%{h}' '\n')
-        fs.write(fr';Params IlfProvider=kukaroboter.arctech.arcoffstandardslin; Kuka.IsGlobalPoint=False; Kuka.PointName=P{i+1}; Kuka.BlendingEnabled=False; Kuka.MoveDataName=CPDAT{i+1}; Kuka.VelocityFieldEnabled=False; Kuka.ColDetectFieldEnabled=True; Kuka.CurrentCDSetIndex=0; Kuka.MovementParameterFieldEnabled=True; IlfCommand=; ArcTech.WdatVarName=WDAT{i+1}; ArcTech.Basic=3.3.3.366; ArcTech.Advanced=3.3.1.22' '\n')
-        fs.write(r';ENDFOLD' '\n')
-        fs.write(fr';TRIGGER WHEN PATH = ArcGetPath(#ArcOffBefore, WDAT{i+1}) DELAY = 0 DO ArcMainNG(#ArcOffBeforeOffSplSingle, WDAT{i+1}, WP{i+1}) PRIO = -1' '\n')
-        fs.write(fr';TRIGGER WHEN PATH = ArcGetPath(#OnTheFlyArcOff, WDAT{i+1}) DELAY = 0 DO ArcMainNG(#ArcOffSplSingle, WDAT{i+1}, WP{i+1}) PRIO = -1' '\n')
-        fs.write(fr';ArcMainNG(#ArcOffBeforeSplSingle, WDAT{i+1}, WP{i+1})' '\n')
-        fs.write(fr'SLIN XP{i+1} WITH $VEL = SVEL_CP(gArcBasVelDefinition, , LCPDAT{i+1}), $TOOL = STOOL2(FP{i+1}), $BASE = SBASE(FP{i+1}.BASE_NO), $IPO_MODE = SIPO_MODE(FP{i+1}.IPO_FRAME), $LOAD = SLOAD(FP{i+1}.TOOL_NO), $ACC = SACC_CP(LCPDAT{i+1}), $ORI_TYPE = SORI_TYP(LCPDAT{i+1}), $APO = SAPO(LCPDAT{i+1}), $JERK = SJERK(LCPDAT{i+1}), $COLLMON_TOL_PRO[1] = USE_CM_PRO_VALUES(0)' '\n')
-        fs.write(fr';ArcMainNG(#ArcOffAfterSplSingle, WDAT{i+1}, WP{i+1})' '\n')
-        fs.write(r';ENDFOLD' '\n\n')
+        # respace the points and include z vals (fixed)
+        interpoints, perimeter, nPts = respace_equally(xycoords, 5.00)
+        interpoints = np.insert(interpoints, 2, A[0,2], axis=1)
 
-        fd.write(fr'DECL stArcDat_T WDAT3={{WdatId[] "WDAT{i+1}",Crater {{JobModeId[] "Job mode",ParamSetId[] "Set3",CraterTime 0.0,PostflowTime 0.0,Channel1 1.00000,Channel2 0.0,Channel3 0.0,Channel4 0.0,Channel5 0.0,Channel6 0.0,Channel7 0.0,Channel8 0.0,BurnBackTime 0.0}},Advanced {{IgnitionErrorStrategy 1,WeldErrorStrategy 1,SlopeOption #None,SlopeTime 0.0,SlopeDistance 0.0,OnTheFlyActiveOn FALSE,OnTheFlyActiveOff FALSE,OnTheFlyDistanceOn 0.0,OnTheFlyDistanceOff 0.0}}}}' '\n')
-        fd.write(fr'DECL stArcDat_T WP{i+1}={{WdatId[] "WP{i+1}",Info {{Version 303030366}}}}' '\n')
-        fd.write(fr'DECL FRAME XP{i+1}={{X {points[0]:.6f},Y {points[1]:.6f},Z {points[2]:.6f},A {tool_angle[0]:.6f},B {tool_angle[1]:.6f},C {tool_angle[2]:.6f}}}' '\n')
-        fd.write(fr'DECL FDAT FP{i+1}={{TOOL_NO 1,BASE_NO 1,IPO_FRAME #BASE,POINT2[] " "}}' '\n')
-        fd.write(fr'DECL LDAT LCPDAT{i+1}={{VEL 2.00000,ACC 100.000,APO_DIST 100.000,APO_FAC 50.0000,AXIS_VEL 100.000,AXIS_ACC 100.000,ORI_TYP #VAR,CIRC_TYP #BASE,JERK_FAC 50.0000,GEAR_JERK 100.000,EXAX_IGN 0}}' '\n\n')
+        # plot
+        plt.plot(interpoints[:,0], interpoints[:,1], '.')
+        #plt.show()
 
-    else:
-        # write arc switch
-        fs.write(fr';FOLD ARCSWI WDAT{i+1} SLIN P{i+1} CPDAT{i+1} Tool[1]:Welder Base[1] ;%{{PE}}' '\n')
-        fs.write(r';FOLD Parameters ;%{h}' '\n')
-        fs.write(fr';Params IlfProvider=kukaroboter.arctech.arcswistandardslin; Kuka.IsGlobalPoint=False; Kuka.PointName=P{i+1}; Kuka.BlendingEnabled=True; Kuka.MoveDataName=CPDAT{i+1}; Kuka.VelocityFieldEnabled=False; Kuka.ColDetectFieldEnabled=True; Kuka.CurrentCDSetIndex=0; Kuka.MovementParameterFieldEnabled=True; IlfCommand=; ArcTech.WdatVarName=WDAT{i+1}; ArcTech.Basic=3.3.3.366; ArcTech.Advanced=3.3.1.22' '\n')
-        fs.write(r';ENDFOLD' '\n')
-        fs.write(fr';TRIGGER WHEN DISTANCE = 1 DELAY = 0 DO ArcMainNG(#ArcSwiSplSingle, WDAT{i+1}, WP{i+1}) PRIO = -1' '\n')
-        fs.write(fr';ArcMainNG(#ArcSwiBeforeSplSingle, WDAT{i+1}, WP{i+1})' '\n')
-        fs.write(fr'SLIN XP{i+1} WITH $VEL = SVEL_CP(gArcBasVelDefinition, , LCPDAT{i+1}), $TOOL = STOOL2(FP{i+1}), $BASE = SBASE(FP{i+1}.BASE_NO), $IPO_MODE = SIPO_MODE(FP{i+1}.IPO_FRAME), $LOAD = SLOAD(FP{i+1}.TOOL_NO), $ACC = SACC_CP(LCPDAT{i+1}), $ORI_TYPE = SORI_TYP(LCPDAT{i+1}), $APO = SAPO(LCPDAT{i+1}), $JERK = SJERK(LCPDAT{i+1}), $COLLMON_TOL_PRO[1] = USE_CM_PRO_VALUES(0) C_Spl' '\n')
-        fs.write(fr';ArcMainNG(#ArcSwiAfterSplSingle, WDAT{i+1}, WP{i+1})' '\n')
-        fs.write(r';ENDFOLD' '\n\n')
+        # for every point write the arc command and position
+        for i, points in enumerate(interpoints):
+            if i == 0: # arc on
+                fs.write(fr';FOLD ARCON WDAT{j} SPTP P{j} Vel=100 % PDAT{j} Tool[1]:Welder Base[1] ;%{{PE}}' '\n')
+                fs.write(r';FOLD Parameters ;%{h}' '\n')
+                fs.write(fr';Params IlfProvider=kukaroboter.arctech.arconstandardsptp; Kuka.IsGlobalPoint=False; Kuka.PointName=P{j}; Kuka.BlendingEnabled=False; Kuka.MoveDataPtpName=PDAT{j}; Kuka.VelocityPtp=100; Kuka.VelocityFieldEnabled=True; Kuka.ColDetectFieldEnabled=True; Kuka.CurrentCDSetIndex=0; Kuka.MovementParameterFieldEnabled=True; IlfCommand=; ArcTech.WdatVarName=WDAT1; ArcTech.Basic=3.3.3.366; ArcTech.Advanced=3.3.1.22' '\n')
+                fs.write(r';ENDFOLD' '\n')
+                fs.write(fr';TRIGGER WHEN DISTANCE = 1 DELAY = ArcGetDelay(#PreDefinition, WDAT{j}) DO ArcMainNG(#PreDefinition, WDAT{j}, WP{j}) PRIO = -1' '\n')
+                fs.write(fr';TRIGGER WHEN DISTANCE = 1 DELAY = ArcGetDelay(#GasPreflow, WDAT{j}) DO ArcMainNG(#GasPreflow, WDAT{j}, WP{j}) PRIO = -1' '\n')
+                fs.write(fr';ArcMainNG(#ArcOnBeforeSplSingle, WDAT{j}, WP{j})' '\n')
+                fs.write(fr'SPTP XP{j} WITH $VEL_AXIS[1] = SVEL_JOINT(100.0), $TOOL = STOOL2(FP{j}), $BASE = SBASE(FP{j}.BASE_NO), $IPO_MODE = SIPO_MODE(FP{j}.IPO_FRAME), $LOAD = SLOAD(FP{j}.TOOL_NO), $ACC_AXIS[1] = SACC_JOINT(PPDAT{j}), $APO = SAPO_PTP(PPDAT{j}), $GEAR_JERK[1] = SGEAR_JERK(PPDAT{j}), $COLLMON_TOL_PRO[1] = USE_CM_PRO_VALUES(0)' '\n')
+                fs.write(fr';ArcMainNG(#ArcOnAfterSplSingle, WDAT{j}, WP{j})' '\n')
+                fs.write(r';ENDFOLD' '\n\n')
 
-        fd.write(fr'DECL stArcDat_T WDAT{i+1}={{WdatId[] "WDAT{i+1}",Weld {{JobModeId[] "Job mode",ParamSetId[] "Set2",Velocity {(tool_speed/60):.6f},Channel1 1.00000,Channel2 0.0,Channel3 0.0,Channel4 0.0,Channel5 0.0,Channel6 0.0,Channel7 0.0,Channel8 0.0}},Weave {{Pattern #None,Length 4.00000,Amplitude 2.00000,Angle 0.0,LeftSideDelay 0.0,RightSideDelay 0.0}},Advanced {{IgnitionErrorStrategy 1,WeldErrorStrategy 1,SlopeOption #None,SlopeTime 0.0,SlopeDistance 0.0,OnTheFlyActiveOn FALSE,OnTheFlyActiveOff FALSE,OnTheFlyDistanceOn 0.0,OnTheFlyDistanceOff 0.0}}}}' '\n')
-        fd.write(fr'DECL stArcDat_T WP{i+1}={{WdatId[] "WP{i+1}",Info {{Version 303030366}}}}' '\n')
-        fd.write(fr'DECL FRAME XP{i+1}={{X {points[0]:.6f},Y {points[1]:.6f},Z {points[2]:.6f},A {tool_angle[0]:.6f},B {tool_angle[1]:.6f},C {tool_angle[2]:.6f}}}' '\n')
-        fd.write(fr'DECL FDAT FP{i+1}={{TOOL_NO 1,BASE_NO 1,IPO_FRAME #BASE,POINT2[] " "}}' '\n')
-        fd.write(fr'DECL LDAT LCPDAT{i+1}={{VEL 2.00000,ACC 100.000,APO_DIST 5.00000,APO_FAC 50.0000,AXIS_VEL 100.000,AXIS_ACC 100.000,ORI_TYP #VAR,CIRC_TYP #BASE,JERK_FAC 50.0000,GEAR_JERK 100.000,EXAX_IGN 0}}' '\n\n')
+                fd.write(fr'DECL stArcDat_T WDAT{j}={{WdatId[] "WDAT1",Strike {{JobModeId[] "Job mode",ParamSetId[] "Set1",StartTime 0.0,PreFlowTime 0.0,Channel1 1.00000,Channel2 0.0,Channel3 0.0,Channel4 0.0,Channel5 0.0,Channel6 0.0,Channel7 0.0,Channel8 0.0,PurgeTime 0.0}},Weld {{JobModeId[] "Job mode",ParamSetId[] "Set2",Velocity {(tool_speed/60):.6f},Channel1 1.00000,Channel2 0.0,Channel3 0.0,Channel4 0.0,Channel5 0.0,Channel6 0.0,Channel7 0.0,Channel8 0.0}},Weave {{Pattern #None,Length 4.00000,Amplitude 2.00000,Angle 0.0,LeftSideDelay 0.0,RightSideDelay 0.0}},Advanced {{IgnitionErrorStrategy 1,WeldErrorStrategy 1,SlopeOption #None,SlopeTime 0.0,SlopeDistance 0.0,OnTheFlyActiveOn FALSE,OnTheFlyActiveOff FALSE,OnTheFlyDistanceOn 0.0,OnTheFlyDistanceOff 0.0}}}}' '\n')
+                fd.write(fr'DECL stArcDat_T WP{j}={{WdatId[] "WP{j}",Info {{Version 303030366}},Strike {{SeamName[] " ",PartName[] " ",SeamNumber 0,PartNumber 0,DesiredLength 0.0,LengthTolNeg 0.0,LengthTolPos 0.0,LengthCtrlActive FALSE}},Advanced {{BitCodedRobotMark 0}}}}' '\n')
+                fd.write(fr'DECL FRAME XP{j}={{X {points[0]:.6f},Y {points[1]:.6f},Z {points[2]:.6f},A {tool_angle[0]:.6f},B {tool_angle[1]:.6f},C {tool_angle[2]:.6f}}}' '\n')
+                fd.write(fr'DECL FDAT FP{j}={{TOOL_NO 1,BASE_NO 1,IPO_FRAME #BASE,POINT2[] " "}}' '\n')
+                fd.write(fr'DECL PDAT PPDAT{j}={{VEL 100.000,ACC 100.000,APO_DIST 500.000,APO_MODE #CDIS,GEAR_JERK 100.000,EXAX_IGN 0}}' '\n\n')
+
+            elif i == len(interpoints)-1: # arc off
+                fs.write(fr';FOLD ARCOFF WDAT{j} SLIN P{j} CPDAT{j} Tool[1]:Welder Base[1] ;%{{PE}}' '\n')
+                fs.write(r';FOLD Parameters ;%{h}' '\n')
+                fs.write(fr';Params IlfProvider=kukaroboter.arctech.arcoffstandardslin; Kuka.IsGlobalPoint=False; Kuka.PointName=P{j}; Kuka.BlendingEnabled=False; Kuka.MoveDataName=CPDAT{j}; Kuka.VelocityFieldEnabled=False; Kuka.ColDetectFieldEnabled=True; Kuka.CurrentCDSetIndex=0; Kuka.MovementParameterFieldEnabled=True; IlfCommand=; ArcTech.WdatVarName=WDAT{j}; ArcTech.Basic=3.3.3.366; ArcTech.Advanced=3.3.1.22' '\n')
+                fs.write(r';ENDFOLD' '\n')
+                fs.write(fr';TRIGGER WHEN PATH = ArcGetPath(#ArcOffBefore, WDAT{j}) DELAY = 0 DO ArcMainNG(#ArcOffBeforeOffSplSingle, WDAT{j}, WP{j}) PRIO = -1' '\n')
+                fs.write(fr';TRIGGER WHEN PATH = ArcGetPath(#OnTheFlyArcOff, WDAT{j}) DELAY = 0 DO ArcMainNG(#ArcOffSplSingle, WDAT{j}, WP{j}) PRIO = -1' '\n')
+                fs.write(fr';ArcMainNG(#ArcOffBeforeSplSingle, WDAT{j}, WP{j})' '\n')
+                fs.write(fr'SLIN XP{j} WITH $VEL = SVEL_CP(gArcBasVelDefinition, , LCPDAT{j}), $TOOL = STOOL2(FP{j}), $BASE = SBASE(FP{j}.BASE_NO), $IPO_MODE = SIPO_MODE(FP{j}.IPO_FRAME), $LOAD = SLOAD(FP{j}.TOOL_NO), $ACC = SACC_CP(LCPDAT{j}), $ORI_TYPE = SORI_TYP(LCPDAT{j}), $APO = SAPO(LCPDAT{j}), $JERK = SJERK(LCPDAT{j}), $COLLMON_TOL_PRO[1] = USE_CM_PRO_VALUES(0)' '\n')
+                fs.write(fr';ArcMainNG(#ArcOffAfterSplSingle, WDAT{j}, WP{j})' '\n')
+                fs.write(r';ENDFOLD' '\n\n')
+
+                fd.write(fr'DECL stArcDat_T WDAT{j}={{WdatId[] "WDAT{j}",Crater {{JobModeId[] "Job mode",ParamSetId[] "Set3",CraterTime 0.0,PostflowTime 0.0,Channel1 1.00000,Channel2 0.0,Channel3 0.0,Channel4 0.0,Channel5 0.0,Channel6 0.0,Channel7 0.0,Channel8 0.0,BurnBackTime 0.0}},Advanced {{IgnitionErrorStrategy 1,WeldErrorStrategy 1,SlopeOption #None,SlopeTime 0.0,SlopeDistance 0.0,OnTheFlyActiveOn FALSE,OnTheFlyActiveOff FALSE,OnTheFlyDistanceOn 0.0,OnTheFlyDistanceOff 0.0}}}}' '\n')
+                fd.write(fr'DECL stArcDat_T WP{j}={{WdatId[] "WP{j}",Info {{Version 303030366}}}}' '\n')
+                fd.write(fr'DECL FRAME XP{j}={{X {points[0]:.6f},Y {points[1]:.6f},Z {points[2]:.6f},A {tool_angle[0]:.6f},B {tool_angle[1]:.6f},C {tool_angle[2]:.6f}}}' '\n')
+                fd.write(fr'DECL FDAT FP{j}={{TOOL_NO 1,BASE_NO 1,IPO_FRAME #BASE,POINT2[] " "}}' '\n')
+                fd.write(fr'DECL LDAT LCPDAT{j}={{VEL 2.00000,ACC 100.000,APO_DIST 100.000,APO_FAC 50.0000,AXIS_VEL 100.000,AXIS_ACC 100.000,ORI_TYP #VAR,CIRC_TYP #BASE,JERK_FAC 50.0000,GEAR_JERK 100.000,EXAX_IGN 0}}' '\n\n')
+
+            else: # arc switch
+                fs.write(fr';FOLD ARCSWI WDAT{j} SLIN P{j} CPDAT{j} Tool[1]:Welder Base[1] ;%{{PE}}' '\n')
+                fs.write(r';FOLD Parameters ;%{h}' '\n')
+                fs.write(fr';Params IlfProvider=kukaroboter.arctech.arcswistandardslin; Kuka.IsGlobalPoint=False; Kuka.PointName=P{j}; Kuka.BlendingEnabled=True; Kuka.MoveDataName=CPDAT{j}; Kuka.VelocityFieldEnabled=False; Kuka.ColDetectFieldEnabled=True; Kuka.CurrentCDSetIndex=0; Kuka.MovementParameterFieldEnabled=True; IlfCommand=; ArcTech.WdatVarName=WDAT{j}; ArcTech.Basic=3.3.3.366; ArcTech.Advanced=3.3.1.22' '\n')
+                fs.write(r';ENDFOLD' '\n')
+                fs.write(fr';TRIGGER WHEN DISTANCE = 1 DELAY = 0 DO ArcMainNG(#ArcSwiSplSingle, WDAT{j}, WP{j}) PRIO = -1' '\n')
+                fs.write(fr';ArcMainNG(#ArcSwiBeforeSplSingle, WDAT{j}, WP{j})' '\n')
+                fs.write(fr'SLIN XP{j} WITH $VEL = SVEL_CP(gArcBasVelDefinition, , LCPDAT{j}), $TOOL = STOOL2(FP{j}), $BASE = SBASE(FP{j}.BASE_NO), $IPO_MODE = SIPO_MODE(FP{j}.IPO_FRAME), $LOAD = SLOAD(FP{j}.TOOL_NO), $ACC = SACC_CP(LCPDAT{j}), $ORI_TYPE = SORI_TYP(LCPDAT{j}), $APO = SAPO(LCPDAT{j}), $JERK = SJERK(LCPDAT{j}), $COLLMON_TOL_PRO[1] = USE_CM_PRO_VALUES(0) C_Spl' '\n')
+                fs.write(fr';ArcMainNG(#ArcSwiAfterSplSingle, WDAT{j}, WP{j})' '\n')
+                fs.write(r';ENDFOLD' '\n\n')
+
+                fd.write(fr'DECL stArcDat_T WDAT{j}={{WdatId[] "WDAT{j}",Weld {{JobModeId[] "Job mode",ParamSetId[] "Set2",Velocity {(tool_speed/60):.6f},Channel1 1.00000,Channel2 0.0,Channel3 0.0,Channel4 0.0,Channel5 0.0,Channel6 0.0,Channel7 0.0,Channel8 0.0}},Weave {{Pattern #None,Length 4.00000,Amplitude 2.00000,Angle 0.0,LeftSideDelay 0.0,RightSideDelay 0.0}},Advanced {{IgnitionErrorStrategy 1,WeldErrorStrategy 1,SlopeOption #None,SlopeTime 0.0,SlopeDistance 0.0,OnTheFlyActiveOn FALSE,OnTheFlyActiveOff FALSE,OnTheFlyDistanceOn 0.0,OnTheFlyDistanceOff 0.0}}}}' '\n')
+                fd.write(fr'DECL stArcDat_T WP{j}={{WdatId[] "WP{j}",Info {{Version 303030366}}}}' '\n')
+                fd.write(fr'DECL FRAME XP{j}={{X {points[0]:.6f},Y {points[1]:.6f},Z {points[2]:.6f},A {tool_angle[0]:.6f},B {tool_angle[1]:.6f},C {tool_angle[2]:.6f}}}' '\n')
+                fd.write(fr'DECL FDAT FP{j}={{TOOL_NO 1,BASE_NO 1,IPO_FRAME #BASE,POINT2[] " "}}' '\n')
+                fd.write(fr'DECL LDAT LCPDAT{j}={{VEL 2.00000,ACC 100.000,APO_DIST 5.00000,APO_FAC 50.0000,AXIS_VEL 100.000,AXIS_ACC 100.000,ORI_TYP #VAR,CIRC_TYP #BASE,JERK_FAC 50.0000,GEAR_JERK 100.000,EXAX_IGN 0}}' '\n\n')
+            j += 1
 
 # last PTP
 fs.write(r';FOLD SPTP HOME Vel=100 % DEFAULT ;%{PE}' '\n')
